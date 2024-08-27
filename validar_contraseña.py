888bc83441f5ef46141e94478bf3556832a920f3
MichelleBlanco <162941760+MichelleBlanco@users.noreply.github.com>
2024-08-26 23:32:02 -0600

    Update validar_contraseña.py
    nuevo comentario

--- validar_contraseña.py
+++ validar_contraseña.py
@@ -1,11 +1,12 @@
 import re
 
-
 def verificarcontraseña(contraseña):
-     if 8 <= len(contraseña) <= 16:
-          if re.search('[a-z]', contraseña) and re.search('[A-Z]', contraseña):
-               if re.search('[0-9]', contraseña):
+    """Verifica que las contraseñas cumplan con los parámetros establecidos
+    """
+    if 8 <= len(contraseña) <= 12:
+           if re.search('[a-z]', contraseña) and re.search('[A-Z]', contraseña):
+                if re.search('[0-9]', contraseña):
                     if re.search('[@$#]', contraseña):
-                         return True
+                        return True
                          
-     return False
+    return False
