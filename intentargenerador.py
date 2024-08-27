112c0369a3b8febf0d122dad50fe6e148134fc9b
MichelleBlanco <162941760+MichelleBlanco@users.noreply.github.com>
2024-08-26 23:32:31 -0600

    Update intentargenerador.py
    Nuevo comentario

--- intentargenerador.py
+++ intentargenerador.py
@@ -1,7 +1,8 @@
 import random
 
-
 def generador():
+    """Se encarga de generar contraseñas aleatorias cada vez que el usuario lo solicite
+    """
     letraminuscula= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     letrasmayusculas = []
     for x in letraminuscula:
